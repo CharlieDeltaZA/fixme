626ccf559c8c38f49c8ab4a348aa659d3cc9185b
Calvin Diogo <calvin@diogo.co.za>
2020-10-13 14:58:09 +0200

    Threads for multiple brokers

--- router/src/main/java/router/Executor.java
+++ router/src/main/java/router/Executor.java
@@ -7,16 +7,76 @@ import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Executor {
 
     private final ArrayList<Integer> brokers = new ArrayList();
     private final PrintWriter marketOut;
     private final BufferedReader marketIn;
-
+    private ExecutorService pool;
+    private FixValidation fix;
+    private Generator genny;
+    
     public Executor(BufferedReader marketIn, PrintWriter marketOut) {
         this.marketIn = marketIn;
         this.marketOut = marketOut;
+        this.fix = new FixValidation();
+        this.genny = new Generator();
+    }
+    
+    private class BrokerHandler implements Runnable {
+        private Socket broker;
+        
+        public BrokerHandler(Socket socket) {
+            this.broker = socket;
+        }
+
+        @Override
+        public void run() {
+            System.out.println("Broker Connected!");
+            try {
+                int ID = genny.genBrokerID(brokers);
+                brokers.add(ID);
+    
+    
+    
+                //// new thread here: (add above id, brokerSocket, marketIn and marketOut into thread class) //////
+    
+                PrintWriter brokerOut = new PrintWriter(broker.getOutputStream(), true);
+                BufferedReader brokerIn = new BufferedReader(new InputStreamReader(broker.getInputStream()));
+    
+                brokerOut.println(ID);
+                System.out.println("Added New Broker: " + ID);
+    
+                while (true) {
+                    String orderMsg = brokerIn.readLine();
+    
+                    if (orderMsg == null) break;  // dunno if this is the correct case
+    
+                    if (fix.validateFix(orderMsg)) {
+                        marketOut.println(orderMsg);
+                        String marketRet = marketIn.readLine();
+    
+                        brokerOut.println(marketRet);
+                    }
+                    else brokerOut.println("Formatting Error!");
+                }
+
+            } catch (Exception e) {
+                System.out.println("Error: " + broker);
+                e.printStackTrace();
+            } finally {
+                try {
+                    broker.close();  // need to check if this is correct to do
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                System.out.println("Broker Closed!");
+            }
+        }
+
     }
 
     // open server for brokers
@@ -24,46 +84,46 @@ public class Executor {
     //                                     -> buy/sell msg sent to market
     //                                     -> sends outcome msg to broker
     public void openServer() {
-        try (
-            ServerSocket serverSocket = new ServerSocket(5000);
-        ) {
-            FixValidation fix = new FixValidation();
+        try (ServerSocket serverSocket = new ServerSocket(5000)) {
+            System.out.println("Broker listening on " + serverSocket.getLocalPort());
+            pool = Executors.newFixedThreadPool(8);
 
             while (true) {
-                Socket broker = serverSocket.accept();
-                Generator genny = new Generator();
-                int ID = genny.genBrokerID(brokers);
-                brokers.add(ID);
+
+                pool.execute(new BrokerHandler(serverSocket.accept()));
+
+                // Socket broker = serverSocket.accept();
+                // Generator genny = new Generator();
+                // int ID = genny.genBrokerID(brokers);
+                // brokers.add(ID);
 
 
 
-                //// new thread here: (add above id, brokerSocket, marketIn and marketOut into thread class) //////
+                // //// new thread here: (add above id, brokerSocket, marketIn and marketOut into thread class) //////
 
-                PrintWriter brokerOut = new PrintWriter(broker.getOutputStream(), true);
-                BufferedReader brokerIn = new BufferedReader(new InputStreamReader(broker.getInputStream()));
+                // PrintWriter brokerOut = new PrintWriter(broker.getOutputStream(), true);
+                // BufferedReader brokerIn = new BufferedReader(new InputStreamReader(broker.getInputStream()));
 
-                brokerOut.println(ID);
-                System.out.println("Added New Broker: " + ID);
+                // brokerOut.println(ID);
+                // System.out.println("Added New Broker: " + ID);
 
-                while (true) {
-                    String orderMsg = brokerIn.readLine();
+                // while (true) {
+                //     String orderMsg = brokerIn.readLine();
 
-                    if (orderMsg == null) break;  // dunno if this is the correct case
+                //     if (orderMsg == null) break;  // dunno if this is the correct case
 
-                    if (fix.validateFix(orderMsg)) {
-                        marketOut.println(orderMsg);
-                        String marketRet = marketIn.readLine();
+                //     if (fix.validateFix(orderMsg)) {
+                //         marketOut.println(orderMsg);
+                //         String marketRet = marketIn.readLine();
 
-                        brokerOut.println(marketRet);
-                    }
-                    else brokerOut.println("Formatting Error!");
-                }
-                broker.close();  // need to check if this is correct to do
+                //         brokerOut.println(marketRet);
+                //     }
+                //     else brokerOut.println("Formatting Error!");
+                // }
+                // broker.close();  // need to check if this is correct to do
                 // stop thread or whatever you using here
 
                 //// end of thread content. //////////////////////
-
-
 
             }
         } catch (IOException e) {
