a93d121407e5702450395b7977c85a05afc5503c
Calvin Diogo <calvin@diogo.co.za>
2020-10-16 15:02:42 +0200

    RouterTable

---
+++ router/src/main/java/router/RouterTable.java
@@ -0,0 +1,10 @@
+package router;
+
+import java.util.ArrayList;
+
+public class RouterTable {
+    
+    private final ArrayList<Integer> brokers = new ArrayList<>();
+    private final ArrayList<Integer> markets = new ArrayList<>();
+
+}
